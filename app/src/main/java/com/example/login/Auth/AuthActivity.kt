046fec7accde72ca1001a6866f304d777ff17e2c
Daniel Christi <danielchristicar@gmail.com>
2025-10-23 16:44:26 -0300

    5 horas de papo com o Alex.

--- app/src/main/java/com/example/login/Auth/AuthActivity.kt
+++ app/src/main/java/com/example/login/Auth/AuthActivity.kt
@@ -5,17 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentTransaction
 import com.example.login.R
+import com.example.login.databinding.ActivityAuthBinding
+import com.example.login.databinding.ActivityMainBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_auth)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Inflar o layout usando o View Binding
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
