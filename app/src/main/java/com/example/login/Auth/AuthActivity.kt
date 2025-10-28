e60ff0b9eff78f0e3d8ff926e96d1d0d28f77929
Daniel Christi <danielchristicar@gmail.com>
2025-10-28 08:55:41 -0300

    erro na execução

--- app/src/main/java/com/example/login/Auth/AuthActivity.kt
+++ app/src/main/java/com/example/login/Auth/AuthActivity.kt
@@ -3,6 +3,7 @@ package com.example.login.Auth
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentTransaction
@@ -15,7 +16,10 @@ class AuthActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_QuickMenu)
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         // Inflar o layout usando o View Binding
         binding = ActivityAuthBinding.inflate(layoutInflater)
